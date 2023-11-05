6117a06531470b803bfce5520d10bfde44646a2d
Olof Blomqvist <olof@twnet.se>
2023-11-05 14:17:01 +0100

    use content-type header for bin version

--- Server/src/bin.rs
+++ Server/src/bin.rs
@@ -1,4 +1,5 @@
 #![feature(start)]
+#![feature(ascii_char)]
 
 mod common;
 use common::*;
@@ -6,6 +7,7 @@ use lsp_types::LogMessageParams;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::io::StdoutLock;
 use std::result::Result::Ok;
 use std::result::Result::Err;
 use std::io::Read;
@@ -27,7 +29,9 @@ fn main() {
     };
 
     let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
     let mut handle = stdin.lock();
+    let mut outhand = stdout.lock();
 
     loop {
         let mut buffer = Vec::new();
@@ -46,14 +50,15 @@ fn main() {
                     }
                     prev_byte = Some(byte[0]);
                 }
-                Err(_) => {
-                    return;
+                Err(e) => {
+                    eprintln!("I dont know what to do with this so just going to ignore it and hope for the best: {e:?}");
+                    return
                 }
             }
         }
 
         let headers_str = String::from_utf8(buffer).unwrap();
-        
+        //eprint!("[SRV] GOT THESE HEADERS FROM CLIENT: {headers_str}");
         let mut headers = HashMap::new();
         for line in headers_str.lines() {
             let parts: Vec<&str> = line.split(": ").collect();
@@ -75,11 +80,13 @@ fn main() {
         handle.read_exact(&mut body).unwrap();
         
         let body_str = String::from_utf8(body).unwrap();
+        //eprintln!("[SRV] message from client: {body_str}");
+
         match server.run_with_json(&body_str) {
-            Ok(Some(v)) => send_msg(&v),
+            Ok(Some(v)) => send_msg(&v, &mut outhand),
             Ok(None) => {},
-            Err(_e) => {
-                // ??
+            Err(e) => {
+                eprintln!("No idea what this is: {:?}",e)
             },
         }
 
@@ -91,7 +98,7 @@ fn main() {
                 "method": "window/logMessage",
                 "params": msg
             });
-            send_msg(&jj);
+            send_msg(&jj, &mut outhand);
         }
 
         while let Some(x) = server.state.outgoing_diagnostics.pop_front() {
@@ -100,15 +107,29 @@ fn main() {
                 "method": "textDocument/publishDiagnostics",
                 "params": x
             });
-           send_msg(&jj);
+           send_msg(&jj,&mut outhand);
         }
         
     }
 }
 
-fn send_msg(message: &Value) {
-    let message_str = serde_json::to_string(message).unwrap();
-    let message_bytes = message_str.as_bytes();
-    std::io::stdout().write_all(message_bytes).unwrap();
-    std::io::stdout().flush().unwrap();
+fn send_msg(message: &Value, stdio: &mut StdoutLock) {
+    
+    if let Ok(message_str) = serde_json::to_string(message) {
+
+        let message_str = message_str.trim();
+        let message_bytes = message_str.as_bytes();
+        let message_length = message_bytes.len();
+       
+        eprintln!("[SRV] sending this to client: {message_str}");
+        let header_str = format!("Content-Length: {}\r\n\r\n", message_length);
+        let header_bytes = header_str.as_ascii().unwrap().as_bytes();
+
+        stdio.write_all(header_bytes).unwrap();
+        stdio.write_all(message_bytes).unwrap();
+        stdio.flush().unwrap();
+    
+    } else {
+        panic!("sorry, there is a bug in marlowe-lsp: failed to serialize a message to json.")
+    }
 }
